1f21c665c7036ec4cf20e11d4f7e302b88bad2e1
Vladimir Eremeev <wl2776@gmail.com>
2021-02-28 06:21:16 +0300

    возня с LCD

--- lesson14_LCD/src/Serial_LCD1602_Display.cpp
+++ lesson14_LCD/src/Serial_LCD1602_Display.cpp
@@ -1,42 +1,52 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+#define BACKLIGHT_PIN   15
+//LiquidCrystal_I2C lcd(0x27, BACKLIGHT_PIN, POSITIVE);  // Set the LCD I2C address
+LiquidCrystal_I2C lcd(0x27);
+
+// Create a set of new characters
+const uint8_t charBitmap[][8] = {
+   { 0xc, 0x12, 0x12, 0xc, 0, 0, 0, 0 },
+   { 0x6, 0x9, 0x9, 0x6, 0, 0, 0, 0 },
+   { 0x0, 0x6, 0x9, 0x9, 0x6, 0, 0, 0x0 },
+   { 0x0, 0xc, 0x12, 0x12, 0xc, 0, 0, 0x0 },
+   { 0x0, 0x0, 0xc, 0x12, 0x12, 0xc, 0, 0x0 },
+   { 0x0, 0x0, 0x6, 0x9, 0x9, 0x6, 0, 0x0 },
+   { 0x0, 0x0, 0x0, 0x6, 0x9, 0x9, 0x6, 0x0 },
+   { 0x0, 0x0, 0x0, 0xc, 0x12, 0x12, 0xc, 0x0 }
+};
 
 void setup()
 {
-    lcd.init();                      // initialize the lcd
-    lcd.backlight();
-    lcd.setCursor(2, 0);  //go to start of 2nd line
-    lcd.print("Hello, world!");
-    lcd.setCursor(4, 1);  //go to start of 2nd line
-    lcd.print("Wl2776");
+    lcd.begin(16,2);
+    //int charBitmapSize = (sizeof(charBitmap ) / sizeof (charBitmap[0]));
+//    for ( int i = 0; i < charBitmapSize; i++ )
+    //{
+        //lcd.createChar ( i, (uint8_t *)charBitmap[i] );
+//    }
 
-    /*Serial.begin (9600); // Leonardo: wait for serial port to connect
-    while (!Serial) { }
-    Serial.println ();
-    Serial.println ("I2C scanner. Scanning ...");
-    byte count = 0;
-    Wire.begin();
-    for (byte i = 8; i < 120; i++) {
-        Wire.beginTransmission (i);
-        if (Wire.endTransmission () == 0) {
-            Serial.print ("Found address: ");
-            Serial.print (i, DEC);
-            Serial.print (" (0x");
-            Serial.print (i, HEX);
-            Serial.println (")");
-            count++;
-            delay (1); // maybe unneeded?
-        } // end of good response
-    } // end of for loop
-    Serial.println ("Done.");
-    Serial.print ("Found ");
-    Serial.print (count, DEC);
-    Serial.println (" device(s).");*/
-    Serial.begin(9600);
+    lcd.home();
+    lcd.print("H");
+    //lcd.setCursor( 0, 1 );        // go to the next line
+    //lcd.print(" FORUM - fm   ");
 }
 
 void loop()
 {
+  ///lcd.home ();
+    //lcd.print("H");
+    //// Do a little animation by writing to the same location
+    //for ( int i = 0; i < 2; i++ )
+    //{
+        //for ( int j = 0; j < 16; j++ )
+        //{
+            //lcd.print (1);
+        //}
+        //lcd.setCursor ( 0, i );
+    //}
+//    delay (200);
 }
+
+
