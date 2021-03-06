ed91c7fb4d1abdc941e356a93abaa149640f2c37
Vladimir Eremeev <wl2776@gmail.com>
2021-03-06 14:36:21 +0300

    can now debug

--- lesson14_LCD/src/Serial_LCD1602_Display.cpp
+++ lesson14_LCD/src/Serial_LCD1602_Display.cpp
@@ -1,10 +1,16 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
+#if DEBUG
+#include <avr8-stub.h>
+#endif
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup()
 {
+#if DEBUG
+    debug_init();
+#endif
     lcd.init();                      // initialize the lcd
     lcd.backlight();
     lcd.setCursor(2, 0);  //go to start of 2nd line
