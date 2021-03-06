4de853149af4b69d0ca080a404513f530588cfc3
Vladimir Eremeev <wl2776@gmail.com>
2021-03-06 18:18:22 +0300

    избавился от delay() в loop

--- lesson7_LCD_T_humidity_sensor/src/Temperature-Humidity_Sensor.cpp
+++ lesson7_LCD_T_humidity_sensor/src/Temperature-Humidity_Sensor.cpp
@@ -14,6 +14,8 @@ const int ledPin =  13;      // номер выхода светодиода
 DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01);
 
+unsigned long last_millis;
+
 void setup() {
     pinMode(ledPin, OUTPUT);
     pinMode(buttonPin, INPUT_PULLUP);
@@ -23,27 +25,32 @@ void setup() {
     dht.begin();
     Serial.begin(9600);
     delay(2000);
+    last_millis = millis();
 }
 
 void loop() {
-    // Reading temperature or humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    float h = dht.readHumidity();
-    float t = dht.readTemperature(is_fahrenheit);
+    if (millis() - last_millis > 200) {
+        // Reading temperature or humidity takes about 250 milliseconds!
+        // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+        float h = dht.readHumidity();
+        float t = dht.readTemperature(is_fahrenheit);
 
-    if (isnan(h) || isnan(t)) {
-        Serial.println(F("Failed to read from DHT sensor!"));
-        lcd.setCursor(13, 1);
-        lcd.print('.');
+        if (isnan(h) || isnan(t))
+        {
+            Serial.println(F("Failed to read from DHT sensor!"));
+            lcd.setCursor(13, 1);
+            lcd.print('.');
+        }
+
+        lcd.setCursor(0, 0);
+        lcd.print(String("Temp: ") + t + ((is_fahrenheit) ? String("F ") : String("'C")));
+
+        lcd.setCursor(0, 1);
+        lcd.print("Hum: ");
+        lcd.print(h);
+        lcd.print("%");
+        last_millis = millis();
     }
-
-    lcd.setCursor(0, 0);
-    lcd.print(String("Temp: ") + t + ((is_fahrenheit) ? String("F ") : String("'C")));
-
-    lcd.setCursor(0, 1);
-    lcd.print("Hum: ");
-    lcd.print(h);
-    lcd.print("%");
 
     int buttonState = digitalRead(buttonPin);
     if (buttonState == HIGH) {
@@ -52,7 +59,4 @@ void loop() {
     } else {
         digitalWrite(ledPin, LOW);
     }
-
-    delay(200);
-
 }
