7cbe779071fa68a4a414d37d40a6ac51ac71796c
Vladimir Eremeev <wl2776@gmail.com>
2021-03-13 19:28:01 +0300

    ультразвуковой сенсор

---
+++ lesson13_ultrasonic/src/ultrasonic.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+const int TrigPin = 2; 
+const int EchoPin = 3;
+float distance;
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(TrigPin, OUTPUT);
+    pinMode(EchoPin, INPUT);
+    Serial.println("Ultrasonic sensor:");
+}
+
+void loop()
+{
+    digitalWrite(TrigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TrigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TrigPin, LOW);
+    distance = pulseIn(EchoPin, HIGH) / 58.00;
+    Serial.print("distance is :");
+    Serial.print(distance);
+    Serial.print("cm");
+    Serial.println();
+    delay(100);
+}
