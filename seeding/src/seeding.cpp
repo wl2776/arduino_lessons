f21d38f96eed78605d954c0f67dac53ec21ab193
Vladimir Eremeev <wl2776@gmail.com>
2021-03-14 20:04:29 +0300

    управляем реле.

--- seeding/src/seeding.cpp
+++ seeding/src/seeding.cpp
@@ -1,11 +1,12 @@
 /* wiring
 
-A0 --- middle potentiometer
+A0 --- middle pin of potentiometer
 A4 --- LCD SDA
 A5 --- LCD SCL
+
 D2 --- button
 D8 --- DHT11
-
+D7 --- relay S pin
 */
 
 #include <Arduino.h>
@@ -20,8 +21,9 @@ const bool is_fahrenheit = false;
 
 const int AD_pin = A0 ;
 
-const int button_pin = 2;     // номер входа, подключенный к кнопке
-const int led_pin =  13;      // номер выхода светодиода
+const int button_pin = 2;
+const int led_pin =  13;
+const int relay_pin = 7;
 
 DHT dht(DHT_pin, DHTTYPE);
 LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01);
@@ -30,13 +32,16 @@ Bounce2::Button button = Bounce2::Button();
 unsigned long DHT_last_millis, AD_last_millis, AD_activity_millis;
 bool reading_pot = false;
 int temperature_threshold = 25;
-int prev_value = 0;
+int temperature_tolerance = 2;
 
+int prev_value = 0;  // prev voltage value from pot, to detect inactivity
 
 void setup() {
     pinMode(AD_pin, INPUT);
     pinMode(led_pin, OUTPUT);
     digitalWrite(led_pin, LOW);
+
+    pinMode(relay_pin, OUTPUT);
 
     lcd.begin(16, 2);
     lcd.backlight();
@@ -52,27 +57,38 @@ void setup() {
 }
 
 void loop() {
+    float current_temperature, current_humidity;
     if (!reading_pot && millis() - DHT_last_millis > 200) {
         // Reading temperature or humidity takes about 250 milliseconds!
         // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
         float h = dht.readHumidity();
         float t = dht.readTemperature(is_fahrenheit);
 
-        if (isnan(h) || isnan(t))
-        {
+        if (isnan(h) || isnan(t)) {
             Serial.println(F("Failed to read from DHT sensor!"));
             lcd.setCursor(13, 1);
             lcd.print('.');
+        } else {
+            lcd.setCursor(0, 0);
+            lcd.print(String("T: ") + t + String("'C (") + temperature_threshold + String(")"));
+
+            lcd.setCursor(0, 1);
+            lcd.print("Hum: ");
+            lcd.print(h);
+            lcd.print("%");
+
+            current_temperature = t;
+            current_humidity = h;
         }
-
-        lcd.setCursor(0, 0);
-        lcd.print(String("T: ") + t + String("'C (") + temperature_threshold + String(")"));
-
-        lcd.setCursor(0, 1);
-        lcd.print("Hum: ");
-        lcd.print(h);
-        lcd.print("%");
         DHT_last_millis = millis();
+    }
+
+    if (current_temperature < temperature_threshold - temperature_tolerance) {
+        digitalWrite(relay_pin, HIGH);
+        digitalWrite(led_pin, HIGH);
+    } else if (current_temperature > temperature_threshold + temperature_tolerance) {
+        digitalWrite(relay_pin, LOW);
+        digitalWrite(led_pin, LOW);
     }
 
     if (reading_pot && millis() - AD_last_millis > 200) {
@@ -102,6 +118,4 @@ void loop() {
             AD_activity_millis = millis();
         }
     }
-    int button_state = digitalRead(button_pin);
-    digitalWrite(led_pin, button_state);
  }
