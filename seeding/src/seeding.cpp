b316dc2a3f2afdd23ad7564e9ad087f0ecf5c83a
Vladimir Eremeev <wl2776@gmail.com>
2021-03-14 19:04:20 +0300

    интерфейс для задания температуры

--- seeding/src/seeding.cpp
+++ seeding/src/seeding.cpp
@@ -3,6 +3,7 @@
 A0 --- middle potentiometer
 A4 --- LCD SDA
 A5 --- LCD SCL
+D2 --- button
 D8 --- DHT11
 
 */
@@ -11,30 +12,45 @@ D8 --- DHT11
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
+#include <Bounce2.h>
 
-const uint8_t DHTPIN = 8;
+const uint8_t DHT_pin = 8;
 const uint8_t DHTTYPE = DHT11;
 const bool is_fahrenheit = false;
 
-const int ADPIN = A0 ;
+const int AD_pin = A0 ;
 
-DHT dht(DHTPIN, DHTTYPE);
+const int button_pin = 2;     // номер входа, подключенный к кнопке
+const int led_pin =  13;      // номер выхода светодиода
+
+DHT dht(DHT_pin, DHTTYPE);
 LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01);
+Bounce2::Button button = Bounce2::Button();
 
 unsigned long DHT_last_millis, AD_last_millis;
+bool reading_pot = false;
+int temperature_threshold = 25;
 
 void setup() {
-    pinMode(ADPIN, INPUT);
+    pinMode(AD_pin, INPUT);
+    pinMode(led_pin, OUTPUT);
+    digitalWrite(led_pin, LOW);
+
     lcd.begin(16, 2);
     lcd.backlight();
     dht.begin();
     Serial.begin(9600);
     delay(2000);
+
+    button.attach(button_pin, INPUT_PULLUP);
+    button.interval(5);
+    button.setPressedState(HIGH); 
+
     DHT_last_millis = AD_last_millis = millis();
 }
 
 void loop() {
-    if (millis() - DHT_last_millis > 200) {
+    if (!reading_pot && millis() - DHT_last_millis > 200) {
         // Reading temperature or humidity takes about 250 milliseconds!
         // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
         float h = dht.readHumidity();
@@ -57,11 +73,23 @@ void loop() {
         DHT_last_millis = millis();
     }
 
-    if (AD_last_millis - millis() > 1000) {
-        int value = analogRead(ADPIN);
-        float voltage = (float)value / 1023.0f * 5;
-        Serial.print("get ad pin value "); Serial.println(value);
-        Serial.print("voltage = "); Serial.println(voltage);
+    if (reading_pot && AD_last_millis - millis() > 200) {
+        int value = analogRead(AD_pin);
+        int temperature = map(value, 0, 1023, 20, 40);
+
+        lcd.setCursor(0, 0);
+        lcd.print(String("Set temp: ") + temperature + String("\'C"));
+
+        // Serial.print("get ad pin value "); Serial.println(value);
+        // Serial.print("voltage = "); Serial.println(voltage);
         AD_last_millis = millis();
     }
-}
+
+    button.update();
+    if (button.pressed()) {
+        reading_pot = !reading_pot;
+        lcd.clear();
+    }
+    int button_state = digitalRead(button_pin);
+    digitalWrite(led_pin, button_state);
+ }
